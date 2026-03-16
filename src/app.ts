e09618a3b4adfa06bd3834922df0e03ca228683a
Danny-1211 <danny21316@gmail.com>
2026-03-17 00:11:26 +0800

    feat: install mongoose and connect mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { config } from './config/env';
-
+import { connectDB } from './utils/db';
 const app = express();
 const port = process.env.PORT || config.PORT;
 app.use(express.json());
 
-app.listen(port , () => {
-    console.log('server is running on port ' + port );
-})
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log('server is running on port ' + port);
+    });
+  } catch (error) {
+    console.error('Failed to start the server:', error);
+  }
+};
+
+startServer();
