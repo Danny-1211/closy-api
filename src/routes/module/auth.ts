dfc938f7c00c04519308085f412714cfc0789901
Danny-1211 <danny21316@gmail.com>
2026-03-19 19:01:31 +0800

    feat: google auth api verion 1

---
+++ src/routes/module/auth.ts
@@ -0,0 +1,46 @@
+// google 登入驗證路由
+import express from 'express';
+import { loginWithGoogle } from '../../services/authServices';
+import { config } from '../../types/env';
+const authRouter = express.Router();
+
+authRouter.post('/google', async (req, res) => {
+  const { id_token } = req.body;
+
+  if (!id_token) {
+    res.status(400).json({ message: 'token is required' });
+    return;
+  }
+
+  try {
+    const { token, user } = await loginWithGoogle(id_token);
+    res.status(200).json({
+      statusCode: 200,
+      status: true,
+      message: "登入成功",
+      data: {
+        token,
+        tokenExpiresIn: config.JWT_EXPIRES_IN,
+        user: {
+          userId: String(user._id),
+          name: user.name,
+          email: user.email,
+          avatar: user.picture,
+          isProfileCompleted: !!user.gender, // gender 有值代表已完成引導頁
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(401).json(
+      {
+        statusCode: 401,
+        status: false,
+        message: "Google token 驗證失敗",
+        data: null,
+      }
+    );
+  }
+});
+
+export { authRouter };
