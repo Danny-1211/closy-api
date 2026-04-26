b4fb45a4f494c3685d5112814fd6d992ed8e6e32
Danny-1211 <danny21316@gmail.com>
2026-04-26 21:11:01 +0800

    Revert "udpate: demo address add cors list"
    This reverts commit 4d829cea44e1d8060284876db3f829c3edc74f74.

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -7,8 +7,7 @@ const corsOptions: CorsOptions = {
       'http://localhost:5173',
       'https://qa-closy-nine.vercel.app',
       'https://closy-api.onrender.com',
-      'https://closy-nine.vercel.app',
-      'https://demo-closy-nine.vercel.app'
+      'https://closy-nine.vercel.app'
     ];
 
     // 允許沒有 origin 的請求（例如 Swagger UI、Postman、curl）
